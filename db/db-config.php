bc034f0ebac005f889c9ced951fcd40068515c28
thomazllr <guithomaz270@gmail.com>
2025-03-12 19:31:36 -0300

    add: config for database

---
+++ db/db-config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '5432',
+        'dbname' => 'postgres', 
+        'user' => 'postgres',
+        'password' => 'postgres' 
+    ]
+];
+
---
+++ db/db.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    public function __construct($host, $port, $username, $password, $dbname) {
+        try {
+            $this->connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Falha ao conectar com o banco: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            $config = require __DIR__ . '/db-config.php';
+            self::$instance = new self(
+                $config['database']['host'],
+                $config['database']['port'],
+                $config['database']['username'],
+                $config['database']['password'],
+                $config['database']['dbname']
+            );
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(){
+         return $this->connection;
+    }
+}
+
+
